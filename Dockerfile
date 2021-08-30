caff394c6b471f2cd4d608b74318e0fe329e1a08
SoloSynth1 <orixay@hotmail.com>
2021-08-31 00:43:34 +0800

    update Dockerfile; fix detectvideo.py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM nvcr.io/nvidia/tensorflow:21.03-tf2-py3
+#FROM nvcr.io/nvidia/tensorflow:19.10-py3
+FROM nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04
 
 COPY . /app
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y libgl1 libglib2.0-0
+RUN apt-get update && apt-get install -y libgl1 libglib2.0-0 libsm6 libxrender1 libfontconfig1 libice6 python3 python3-pip
 
-RUN pip install -r requirements-docker.txt
+RUN python3 -m pip install -U pip
 
-ENTRYPOINT ["python"]
+RUN pip3 install -r requirements-gpu.txt
+
+ENTRYPOINT ["python3", "detectvideo.py"]
--- detectvideo.py
+++ detectvideo.py
@@ -1,18 +1,15 @@
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+for p_device in physical_devices:
+    tf.config.experimental.set_memory_growth(p_device, True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
 from core.yolov4 import filter_boxes
-from tensorflow.python.saved_model import tag_constants
-from PIL import Image
 import cv2
 import numpy as np
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.python.saved_model import tag_constants
 
 flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
@@ -30,14 +27,10 @@ flags.DEFINE_boolean('dis_cv2_window', True, 'disable cv2 window during the proc
 
 
 def main(_argv):
-    # config = ConfigProto()
-    # config.gpu_options.allow_growth = True
-    # session = InteractiveSession(config=config)
-    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
     input_size = FLAGS.size
     video_path = FLAGS.video
 
-    print("Video from: ", video_path )
+    logging.info("Video from: ", video_path)
     vid = cv2.VideoCapture(video_path)
 
     if FLAGS.framework == 'tflite':
@@ -45,12 +38,17 @@ def main(_argv):
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
-        print(input_details)
-        print(output_details)
+        logging.info(input_details)
+        logging.info(output_details)
     else:
-        # saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_c   onstants.SERVING])
+        # saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
         # infer = saved_model_loaded.signatures['serving_default']
-        saved_model_loaded = tf.keras.models.load_model(FLAGS.weights, compile=False)
+
+        # # tf saved_model style
+        saved_model = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+
+        # keras model style
+        # saved_model_loaded = tf.keras.models.load_model(FLAGS.weights, compile=False)
     
     if FLAGS.output:
         # by default VideoCapture returns float instead of int
@@ -65,14 +63,14 @@ def main(_argv):
         return_value, frame = vid.read()
         if return_value:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(frame)
         else:
+            logging.info(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+            logging.info(frame_id)
             if frame_id == vid.get(cv2.CAP_PROP_FRAME_COUNT):
-                print("Video processing complete")
+                logging.info("Video processing complete")
                 break
             raise ValueError("No image! Try with another video format")
-        
-        frame_size = frame.shape[:2]
+
         image_data = cv2.resize(frame, (input_size, input_size))
         image_data = image_data / 255.
         image_data = image_data[np.newaxis, ...].astype(np.float32)
@@ -91,13 +89,16 @@ def main(_argv):
         else:
             # batch_data = tf.constant(image_data)
             # pred_bbox = infer(batch_data)
-            pred_bbox = saved_model_loaded.predict(image_data)
-            # print(pred_bbox.keys())
-            # for key, value in pred_bbox.items():
-            #     boxes = value[:, :, 0:4]
-            #     pred_conf = value[:, :, 4:]
+
+            # tf saved_model style
+            pred_bbox = saved_model(image_data)
             boxes = pred_bbox[:, :, 0:4]
             pred_conf = pred_bbox[:, :, 4:]
+
+            # # keras model style
+            # pred_bbox = saved_model_loaded.predict(image_data)
+            # boxes = pred_bbox[:, :, 0:4]
+            # pred_conf = pred_bbox[:, :, 4:]
 
         boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
             boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
